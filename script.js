3083f80eb399c866a801dd65276a359dfaf978c7
Httphameed <Abdulhameedn7@gmail.com>
2026-05-06 11:51:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function uploadFile() {
     const fileInput = document.getElementById('fileInput');
     const file = fileInput.files[0];
-    const logicAppUrl = "PASTE_YOUR_LOGIC_APP_URL_HERE";
+    const logicAppUrl = "https://prod-28.italynorth.logic.azure.com:443/workflows/aecb1b0cbf744b57a4118bddb4655d74/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=FcTTR_wLtJLhYN0n3hCK904DO7I1KF-7FxV3omK83oo";
 
     const reader = new FileReader();
     reader.onloadend = async () => {
